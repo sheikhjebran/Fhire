b20331519e6aedd2ddd5fc70bd565e8db250b3fc
jebran.sheikh@philips.com <sheikhjebran@gmail.com>
2022-04-03 14:07:01 +0530

    Update test_patient.py

--- fhire/tests/test_fhire_patient/test_patient.py
+++ fhire/tests/test_fhire_patient/test_patient.py
@@ -5,4 +5,4 @@ from fhire.classes.fhire import Fhire
 class TestPatientGetResponse(unittest.TestCase):
 
     def test_get_patient_response(self):
-        Fhire("").parse_https_post_patient_response()
+        Fhire("").parse_https_post_patient_response
